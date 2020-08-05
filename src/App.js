b3db376a9312b302829d4c9c17caef9e5528dfc6
Neto <quintarion@gmail.com>
2020-08-05 09:47:19 +0200

    add Google Map Iframe into ContactInfo file, try to add ReCaptcha and Meta CSP

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+//import ReCAPTCHA from "react-google-recaptcha";
 import Adult from './components/cathegories/Adult';
 import Career from './components/about/Career';
 import Who from './components/cathegories/Who';
@@ -12,38 +13,86 @@ import Training from './components/about/Training';
 
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="site-header"><Header/></div>
-      <div className="site-content">
-        <Gretting/>
-        <Who/>
-        <Training/>
-        <Career/>
-        <Adult/>
-        <Couple/>
-        <Child/>
-        <Contact/>
-      </div>
-      <div className="site-footer"><Footer/></div>
+// const TEST_SITE_KEY = "6Lcla7oZAAAAAIKh5o4dv9HdCYIantCOo5peI0VT";
+// const DELAY = 1500;
+
+class App extends React.Component {
+//   constructor(props, ...args) {
+//     super(props, ...args);
+//     this.state = {
+//       callback: "not fired",
+//       value: "[empty]",
+//       load: false,
+//       expired: "false"
+//     };
+//     this._reCaptchaRef = React.createRef();
+//   }
+
+//   handleChange = value => {
+//     console.log("Captcha value:", value);
+//     this.setState({ value });
+//     /* if value is null recaptcha expired */
+//     if (value === null) this.setState({ expired: "true" });
+//   };
+
+//   asyncScriptOnLoad = () => {
+//     this.setState({ callback: "called!" });
+//     console.log("scriptLoad - reCaptcha Ref-", this._reCaptchaRef);
+//   };
+
+  render() {
+    //const { value, callback, load, expired } = this.state || {};
+    return (
+      <div className="App">
+        <div className="site-header"><Header/></div>
+        <div className="site-content">
+          <Gretting/>
+          <Who/>
+          <Training/>
+          <Career/>
+          <Adult/>
+          <Couple/>
+          <Child/>
+          <Contact/>
+        </div>
+        <div className="site-footer"><Footer/></div>
+        
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
+
+        {/* <h2>
+          NOTE: initial load delayed <em>{DELAY / 1000}sec</em> to demonstrate
+          callback
+        </h2>
+        <h3>Recaptcha loaded callback: {callback}</h3>
+        <h5>Recaptcha value: {value}</h5>
+        <h5>Expired: {expired}</h5>
       
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      {load && (
+        <ReCAPTCHA
+          style={{ display: "inline-block" }}
+          theme="dark"
+          ref={this._reCaptchaRef}
+          sitekey={TEST_SITE_KEY}
+          onChange={this.handleChange}
+          asyncScriptOnLoad={this.asyncScriptOnLoad}
+        />
+      )} */}
     </div>
-  );
+    );
+  }
 }
 
 export default App;
